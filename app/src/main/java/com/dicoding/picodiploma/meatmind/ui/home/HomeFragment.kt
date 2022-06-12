1f6bae08af163d030dc26b7ad9431d65e326ec95
robby <eureka.robby09@gmail.com>
2022-06-13 05:17:54 +0700

    project capstone

--- app/src/main/java/com/dicoding/picodiploma/meatmind/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dicoding/picodiploma/meatmind/ui/home/HomeFragment.kt
@@ -1,106 +1,47 @@
 package com.dicoding.picodiploma.meatmind.ui.home
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.dicoding.picodiploma.meatmind.R
 import com.dicoding.picodiploma.meatmind.camera.CameraActivity
 import com.dicoding.picodiploma.meatmind.databinding.FragmentHomeBinding
-import java.io.File
 
-class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding?=null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private var _binding : FragmentHomeBinding?=null
     private val binding get() = _binding!!
-
-    companion object {
-        const val CAMERA_X_RESULT = 200
-
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-        private const val REQUEST_CODE_PERMISSIONS = 10
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (!allPermissionsGranted()) {
-                Toast.makeText(
-                    requireActivity(),
-                    "Tidak mendapatkan permission.",
-                    Toast.LENGTH_SHORT
-                ).show()
-                requireActivity().finish()
-            }
-        }
-    }
-
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        if (!allPermissionsGranted()) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                REQUIRED_PERMISSIONS,
-                REQUEST_CODE_PERMISSIONS
-            )
-        }
-
-        binding.ScanButton.setOnClickListener { startCameraX() }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        _binding = FragmentHomeBinding.inflate(inflater,container,false)
+
+        return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    private fun startCameraX() {
-        val intent = Intent(requireActivity(), CameraActivity::class.java)
-        launcherIntentCameraX.launch(intent)
-    }
+        (activity as AppCompatActivity).supportActionBar?.hide()
 
-
-    private val launcherIntentCameraX = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) {
-        if (it.resultCode == CAMERA_X_RESULT) {
-            val myFile = it.data?.getSerializableExtra("picture") as File
-            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
-
-            val result = rotateBitmap(
-                BitmapFactory.decodeFile(myFile.path),
-                isBackCamera
-            )
+        binding.ScanButton.setOnClickListener{
+            val intent = Intent(requireActivity(),CameraActivity::class.java)
+            startActivity(intent)
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
+
+
+
